6e54ea68724350e930e16d58bf076a5981674b58
fajarnugroho25 <47359808+fajarnugroho25@users.noreply.github.com>
2023-04-08 09:00:22 +0700

    8 April 2023 09:00

--- application/modules/bpjs/views/vdatabpjs.php
+++ application/modules/bpjs/views/vdatabpjs.php
@@ -17,13 +17,13 @@
         <tr>      
         <th>No Reg</th>
           <th>RM</th>
-          <th>Nama Pasien</th>     
+          <th>Nama Pasien</th>
           <th>DPJP</th>
-          <th>Tagihan</th> 
-          <th>InacBG</th> 
-          <th>ICD IX</th> 
-          <th>ICD X</th> 
-          <th>Catatan</th> 
+          <th>Tagihan</th>
+          <th>Grouping</th>
+          <th>Iur</th>
+          <th>Selisih Tagihan</th>
+          <th>Kelas</th>
           <th>Action</th> 
           </thead>
       <tbody>
@@ -35,10 +35,19 @@
           <td ><?php echo $d->dpjp; ?></td>
           <td style="text-align: right;"><?php echo number_format($d->tagihan); ?></td>
           <td style="text-align: right;"><?php echo number_format($d->grouping); ?></td>
-          <td><?php echo $d->icdix; ?></td>
-          <td><?php echo $d->icdx; ?></td>
-          <td><?php echo $d->catatan; ?></td>
-          
+          <td style="text-align: right;"><?php echo number_format($d->iur); ?></td>
+          <?php
+          $cek = @(($d->selisih_tagihan / $tagihan) * 100 );
+          if ($cek < 100) {
+            echo ' <td style=" text-align: right; ">' . number_format($d->selisih_tagihan) . '</td>';
+          } else if ($cek > 100) {
+            echo  ' <td style=" text-align: right; background-color:#7FFF00; color:black; border-radius: 4px;">' . number_format($d->selisih_tagihan) . '</td>';           
+          } else {
+            echo ' <td style=" text-align: right; background-color:#DC143C; color:white; border-radius: 4px;">' . number_format($d->selisih_tagihan) . '</td>';            
+          }
+          ?>
+          <td><?php echo $d->kelas; ?></td>
+            
           <td align="center">
                                         <a href="<?php echo base_url();?>bpjs/editdatabpjs/<?php echo $d->no_reg; ?>">
                                         <input type="button" value="Edit" class="btn btn-info"></a>
